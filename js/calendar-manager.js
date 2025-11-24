29a2c90803882c1c9200a09aea8faa5c7c68f591
Andrey K. <hixdrew@gmail.com>
2025-11-24 12:56:26 +0300

    upd

--- js/calendar-manager.js
+++ js/calendar-manager.js
@@ -115,7 +115,7 @@ const CalendarManager = {
         {
             id: 13,
             name: 'test',
-            date: '2025-11-24',
+            date: '2025-11-25',
             type: 'notification',
             message: '📅 TEST DR'
         }
